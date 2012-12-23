008dd4115a0df189dc2ce7cf5c06376112eaeb36
Dominic Steinitz <dominic@steinitz.org>
2012-12-23 09:49:44 +0000

    Implement the implicit Euler method. Sadly this does not give the right answer since we seem to be stepping in the space direction rather than the time direction!

--- Conway.hs
+++ Conway.hs
@@ -4,6 +4,8 @@ import Data.Array.Repa
 import Data.Array.Repa.Stencil
 import Data.Array.Repa.Stencil.Dim2
 
+import Debug.Trace
+
 sten :: Stencil DIM2 Int
 sten = [stencil2| 1 1 1
                   1 0 1
@@ -24,10 +26,10 @@ r = 0.05
 sigma = 0.2
 k = 50.0
 t = 3.0
-m = 80
+m = 3
 xMax = 150
 deltaX = xMax / (fromIntegral m)
-n = 800
+n = 3
 deltaT = t / (fromIntegral n)
 
 data PointedArray a = PointedArray Int (Array U DIM1 a)
@@ -47,31 +49,23 @@ priceAtT = PointedArray 0 (fromListUnboxed (Z :. m+1)
                            [ max 0 (deltaX * (fromIntegral j) - k) | j <- [0..m] ])
 
 
-type Grid = Array D DIM2 Double
-
-{-# INLINE relaxLaplace #-}
-relaxLaplace :: Grid -> Grid
-relaxLaplace arr
+{-# INLINE solve #-}
+solve arr
  = traverse arr id elemFn
  where  _ :. height :. width = extent arr
         {-# INLINE elemFn #-}
         elemFn get d@(Z :. i :. j)
-         = if isBorder i j
-           then
-             get d
-           else
-             a * (get (Z :. (i-1) :. (j-1))) +
-             b * (get (Z :. i     :. (j-1))) +
-             c * (get (Z :. (i+1) :. (j-1)))
+          | i == 0      = trace ("1: " Prelude.++ show i Prelude.++ show j) 0.0
+          | i == m {- height -} = trace ("2: " Prelude.++ show i Prelude.++ show j Prelude.++ show (xMax - k)) xMax - k
+          | j == 0      = trace ("3: " Prelude.++ show i Prelude.++ show j) max 0 (deltaX * (fromIntegral j) - k)
+          | otherwise   = trace ("4: " Prelude.++ show i Prelude.++ show j) a * (get (Z :. (i-1) :. (j-1))) +
+                          b * (get (Z :. i     :. (j-1))) +
+                          c * (get (Z :. (i+1) :. (j-1)))
           where
             a = deltaT * (sigma^2 * (fromIntegral i)^2 - r * (fromIntegral i)) / 2
             b = 1 - deltaT * (r  + sigma^2 * (fromIntegral i)^2)
             c = deltaT * (sigma^2 * (fromIntegral i)^2 + r * (fromIntegral i)) / 2
-        {-# INLINE isBorder #-}
-        isBorder i j =
-             (i == 0)
-          || (i >= width - 1)
-          || (j == 0)
-          || (j >= height - 1)
 
+initGrid :: Array U DIM2 Double
+initGrid = fromListUnboxed (Z :. (m+1 :: Int) :. (n+1 :: Int)) (take ((n+1) * (m+1)) $ repeat 0.0)
            
